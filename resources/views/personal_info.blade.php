9057e5f3e10ca47c4f087e283cf2a5c3a2ef6ea1
Dilnaz <dilnaz@MacBook-Air-Dilnaz-2.local>
2021-02-22 11:20:50 +0600

    initial commit

---
+++ resources/views/personal_info.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>My name is Dilnaz Baibeisenova</h1>
+    <h2>ID NUMBER-190103019</h2>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return "Enter /user for getting name <br>  enter /newView for getting new view <br>enter /newView2 for getting second view";
+    
+});
+Route::get('user/{age?}', function () {//enter /user for getting name
+    return "Dilnaz Baibeisenova <br> enter /newView for getting new view <br>enter /newView2 for getting second view";
+});
+Route::get('/newView', function () {//enter /newView for getting new view which called "personal_info" 
+    return view('personal_info');
+});
+Route::get('/newView2', function () {//enter /newView2 for getting new view which called "personal_info2" 
+    return view('personal_info2');
+});
